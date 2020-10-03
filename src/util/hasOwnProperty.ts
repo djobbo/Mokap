36d4bd58f5cb166b05cf2acd747aff0ac9cec376
Alfie <djobbo.maiga@gmail.com>
2020-10-03 12:23:32 +0200

    remove hasOwnProperty

--- src/util/hasOwnProperty.ts
+++
@@ -1,6 +0,0 @@
-export const hasOwnProperty = <X extends Record<PropertyKey, unknown>, Y extends PropertyKey>(
-    obj: X,
-    prop: Y,
-): obj is X & Record<Y, unknown> => {
-    return Object.prototype.hasOwnProperty.call(obj, prop);
-};
