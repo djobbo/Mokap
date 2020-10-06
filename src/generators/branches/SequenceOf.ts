70d0329177c8e6e0659b5e263ee731a713fa505d
Alfie <djobbo.maiga@gmail.com>
2020-10-06 23:56:45 +0200

    seqOf

--- src/generators/branches/SequenceOf.ts
+++ src/generators/branches/SequenceOf.ts
@@ -1,11 +1,5 @@
 import { mok } from '../types';
 import { getMok } from '..';
 
-type ArrayMok<T> = {
-    [P in keyof T]?: mok<P>;
-};
-
-export const mockSequenceOf = <T extends any[]>(...value: ArrayMok<T>) => (...indexes: number[]) =>
-    value.map((x) => getMok(x, ...indexes));
-
-const m = mockSequenceOf(() => 1, 2, 3, '4');
+export const mockSequenceOf = <T extends any[]>(...value: { [P in keyof T]: mok<T[P]> }) => (...indexes: number[]): T =>
+    value.map((x) => getMok(x, ...indexes)) as any;
